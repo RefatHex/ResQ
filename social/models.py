1636ec53642c6491aac6adfaf80f7e1a427773fd
refathex <khondakar00@gmail.com>
2025-03-13 02:46:20 +0600

    added hazard models

--- social/models.py
+++ social/models.py
@@ -1,6 +1,9 @@
 import uuid
 from django.db import models
 
+from location.models import Location
+from users.models import User
+
 class SocialPost(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     PLATFORM_CHOICES = [
@@ -17,3 +20,19 @@ class SocialPost(models.Model):
 
     def __str__(self):
         return f"{self.platform} - {self.timestamp}"
+    
+    
+class Hazard(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    hazard_type = models.CharField(max_length=20)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [models.Index(fields=['hazard_type', 'timestamp'])]
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return f"{self.hazard_type} - {self.location}"
