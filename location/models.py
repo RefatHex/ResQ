488bb09967f5a95ff7b835e3ee53cab6c8c238dd
refathex <khondakar00@gmail.com>
2025-03-13 01:23:28 +0600

    added locations model

--- location/models.py
+++ location/models.py
@@ -1,3 +1,19 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+class Location(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='locations')
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['user', 'timestamp']),  # Index for faster queries
+        ]
+
+    def __str__(self):
+        return f"{self.user.username} - {self.timestamp}"
